fd22f4f72ffcf8d46791ea001c3092d0fa3847cf
Akshit Lalit <akshit.lalit@gmail.com>
2023-06-05 00:27:45 +0530

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require("express");
 const multer = require("multer");
 const bodyParser = require("body-parser");
@@ -8,10 +9,12 @@ const session = require("express-session");
 const pdfPageCounter = require("pdf-page-counter");
 const path = require("path");
 
+// Variables Initialized
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
+// Express Session- config
 app.use(
   session({
     secret: "SECRETKEY123",
@@ -22,6 +25,7 @@ app.use(
   })
 );
 const merger = new PDFMerger();
+//Multer- path and new names of incoming files are configured
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -32,24 +36,13 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.post("/upload-pdf", upload.array("pdf-files"), function (req, res, next) {
-  const files = req.files;
-  const mergedFilePath = path.join(__dirname, "merged.pdf");
-  (async () => {
-    for (let file of files) {
-      console.log(file);
-      await merger.add(file.path);
-    }
-    await merger.save("merged.pdf");
-    for (let file of files) {
-      await fsp.unlink(file.path);
-    }
-    res.render("success");
-  })();
-});
+// POST REQUESTS
 
+// PDFs files are loaded onto the server
+// PDF info array is stored and info about the number of pages is added asynchronously
 app.post("/", upload.array("pdf-files"), (req, res) => {
   const pdfFiles = req.files;
+  // IIFE invoked as current scope cannot be async
   (async () => {
     for (let i = 0; i < pdfFiles.length; i++) {
       let dataBuffer = fs.readFileSync(pdfFiles[i].path);
@@ -61,34 +54,44 @@ app.post("/", upload.array("pdf-files"), (req, res) => {
   })();
 });
 
+// PDF info array and page numbers arrays is read from the current session
+// each PDF is added one by one w.r.t their page numbers
 app.post("/filters", (req, res) => {
   const pdfFiles = req.session.pdfFilesInfo;
   const startingPageNumbers = req.body.startingPageNumbers;
   const endingPageNumbers = req.body.endingPageNumbers;
+  // IIFE invoked because current scope can't be async
   (async () => {
+    // Concatenating all files one by one
     for (let i = 0; i < pdfFiles.length; i++) {
       const range = startingPageNumbers[i] + "-" + endingPageNumbers[i];
-      console.log(range);
       await merger.add(pdfFiles[i].path, range);
     }
     await merger.save("merged.pdf");
     for (let i = 0; i < pdfFiles.length; i++) {
+      // Deleting files as no longer needed
       await fsp.unlink(pdfFiles[i].path);
     }
+    // Buffer is reset as sometimes previous session files were loaded
     merger.reset();
     res.render("success");
   })();
 });
 
+// GET REQUESTS
+// PDF files information object array is read from the current session
+// The array is used to render the filter route.
 app.get("/filters", (req, res) => {
   const pdfFiles = req.session.pdfFilesInfo;
   res.render("filters", { pdfFiles: pdfFiles });
 });
 
+// Download process is started
 app.get("/downloadpdf", (req, res) => {
   res.download(path.join(__dirname, "merged.pdf"));
 });
 
+// Opens the generated PDF in the same tab.
 app.get("/openpdf", (req, res) => {
   res.sendFile(path.join(__dirname, "merged.pdf"));
   // (async () => {
@@ -96,6 +99,7 @@ app.get("/openpdf", (req, res) => {
   // })();
 });
 
+// Initial landing page
 app.get("/", (req, res) => {
   res.render("index");
 });
