959bee5b717a5721e1088b7a096e913ed6e50f0b
Akshit Lalit <akshit.lalit@gmail.com>
2023-06-24 19:01:00 +0530

    old

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fsp = require("fs/promises");
 const fs = require("fs");
 const PDFMerger = require("pdf-merger-js");
 const session = require("express-session");
-// const pdfPageCounter = require("pdf-page-counter");
+const pdfPageCounter = require("pdf-page-counter");
 const path = require("path");
 
 // Variables Initialized
@@ -52,10 +52,9 @@ app.post("/", upload.array("pdf-files"), async (req, res) => {
   await (async () => {
     for (let i = 0; i < pdfFiles.length; i++) {
       // console.log(path.join(__dirname, pdfFiles[i].path));
-      // let dataBuffer = fs.readFileSync(pdfFiles[i].path);
-      // let fileData = await pdfPageCounter(dataBuffer);
-      // pdfFiles[i]["pages"] = fileData.numpages;
-      pdfFiles[i]["pages"] = 10;
+      let dataBuffer = fs.readFileSync(pdfFiles[i].path);
+      let fileData = await pdfPageCounter(dataBuffer);
+      pdfFiles[i]["pages"] = fileData.numpages;
     }
     req.session.pdfFilesInfo = pdfFiles;
     res.redirect("/filters");
@@ -77,9 +76,7 @@ app.post("/filters", (req, res) => {
       const range = startingPageNumbers[i] + "-" + endingPageNumbers[i];
       await merger.add(pdfFiles[i].path, range);
     }
-    // const mergedPdfBuffer = await merger.saveAsBuffer();
-    // fs.writeFileSync(`/tmp/merged.pdf`, mergedPdfBuffer);
-    await merger.save(`/tmp/merged.pdf`);
+    await merger.save("merged.pdf");
     for (let i = 0; i < pdfFiles.length; i++) {
       // Deleting files as no longer needed
       await fsp.unlink(pdfFiles[i].path);
@@ -109,7 +106,7 @@ app.get("/downloadpdf", (req, res) => {
 
 // Opens the generated PDF in the same tab.
 app.get("/openpdf", (req, res) => {
-  res.sendFile("/tmp/merged.pdf");
+  res.sendFile(path.join(__dirname, "merged.pdf"));
   // (async () => {
   //   await fsp.unlink("merged.pdf");
   // })();
@@ -117,7 +114,6 @@ app.get("/openpdf", (req, res) => {
 
 // Initial landing page
 app.get("/", (req, res) => {
-  console.log(require.main.filename);
   res.render("index");
 });
 
