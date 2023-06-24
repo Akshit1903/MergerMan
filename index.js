3dcee120158c9603c3cafa767ec7191b43cc9e3a
Akshit Lalit <akshit.lalit@gmail.com>
2023-06-24 18:55:09 +0530

    working 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fsp = require("fs/promises");
 const fs = require("fs");
 const PDFMerger = require("pdf-merger-js");
 const session = require("express-session");
-const pdfPageCounter = require("pdf-page-counter");
+// const pdfPageCounter = require("pdf-page-counter");
 const path = require("path");
 
 // Variables Initialized
@@ -52,9 +52,10 @@ app.post("/", upload.array("pdf-files"), async (req, res) => {
   await (async () => {
     for (let i = 0; i < pdfFiles.length; i++) {
       // console.log(path.join(__dirname, pdfFiles[i].path));
-      let dataBuffer = fs.readFileSync(pdfFiles[i].path);
-      let fileData = await pdfPageCounter(dataBuffer);
-      pdfFiles[i]["pages"] = fileData.numpages;
+      // let dataBuffer = fs.readFileSync(pdfFiles[i].path);
+      // let fileData = await pdfPageCounter(dataBuffer);
+      // pdfFiles[i]["pages"] = fileData.numpages;
+      pdfFiles[i]["pages"] = 10;
     }
     req.session.pdfFilesInfo = pdfFiles;
     res.redirect("/filters");
