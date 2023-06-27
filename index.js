f29214b6b62a2984aaecffd7c71fb4dfdf2ca9f6
Akshit Lalit <akshit.lalit@gmail.com>
2023-06-28 00:27:48 +0530

    added gridfs

--- index.js
+++ index.js
@@ -9,26 +9,61 @@ const session = require("express-session");
 const path = require("path");
 const countPages = require("page-count");
 const serverless = require("serverless-http");
+// for managing sessions
 const MongoDBStore = require("connect-mongodb-session")(session);
+// for storing files
+const { MongoClient } = require("mongodb");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const mongoose = require("mongoose");
+const { Console, log } = require("console");
+var ObjectId = require("mongodb").ObjectID;
+require("dotenv").config();
 // Variables Initialized
 const app = express();
+mongoose.set("strictQuery", true);
 // configure MongoDB Database
-// const store = new MongoDBStore({
-//   uri: "mongodb+srv://akshit19:sGUuwKTtlK3znU4R@mergermancluster.3l1agd0.mongodb.net/",
-//   collection: "sessions",
+const store = new MongoDBStore({
+  // uri: "mongodb://localhost:27017/user-session-db",
+  uri: `mongodb+srv://${process.env.MONGO_USERNAME_PASSWORD}@mergermancluster.3l1agd0.mongodb.net/user-session-db`,
+  collection: "sessions",
+});
+// const client = new MongoClient("mongodb://localhost:27017/user-files-db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
+// const mongoStorageURI = "mongodb://localhost:27017/user-files-db";
+const mongoStorageURI = `mongodb+srv://${process.env.MONGO_USERNAME_PASSWORD}@mergermancluster.3l1agd0.mongodb.net/user-files-db`;
+mongoose.connect(mongoStorageURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+let bucket;
+const conn = mongoose.connection.on("connected", () => {
+  var db = mongoose.connections[0].db;
+  bucket = new mongoose.mongo.GridFSBucket(db, {
+    bucketName: "uploads",
+  });
+});
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
 var sess = {
-  secret: "SECRETKEY123",
+  secret: process.env.SECRET_KEY,
   cookie: { maxAge: 36000000 },
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  // store: store,
+  store: store,
 };
 if (app.get("env") === "production") {
   app.set("trust proxy", 1); // trust first proxy
   sess.cookie.secure = true; // serve secure cookies
 }
+
 // Express Session- config
 app.use(session(sess));
 app.set("view engine", "ejs");
@@ -38,7 +73,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const merger = new PDFMerger();
 //Multer- path and new names of incoming files are configured
-const storage = multer.diskStorage({
+
+// This code saves the files on disk
+const storageOnDisk = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "/tmp");
   },
@@ -46,21 +83,86 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
-const upload = multer({ storage: storage });
+
+// This code saves the files on mongoDB server
+const storageOnServer = new GridFsStorage({
+  url: mongoStorageURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      const filename = Date.now() + "-" + file.originalname;
+      const fileInfo = {
+        filename: filename,
+        bucketName: "uploads",
+      };
+      resolve(fileInfo);
+    });
+  },
+});
+const upload = multer({ storage: storageOnServer });
+
+// pdfFile structure
+// {
+//   fieldname: 'pdf-files',
+//   originalname: 'Untitled document - Copy.pdf',
+//   encoding: '7bit',
+//   mimetype: 'application/pdf',
+//   destination: '/tmp',
+//   filename: '1687878783277-Untitled document - Copy.pdf',
+//   path: '\\tmp\\1687878783277-Untitled document - Copy.pdf',
+//   size: 16323
+// }
+// {
+//   fieldname: 'pdf-files',
+//   originalname: 'Untitled document - Copy - Copy (2).pdf',
+//   encoding: '7bit',
+//   mimetype: 'application/pdf',
+//   id: new ObjectId("649b04138d95ecaa2ac029bb"),
+//   filename: 'Untitled document - Copy - Copy (2).pdf',
+//   metadata: null,
+//   bucketName: 'uploads',
+//   chunkSize: 261120,
+//   size: 16323,
+//   md5: undefined,
+//   uploadDate: 2023-06-27T15:45:23.104Z,
+//   contentType: 'application/pdf'
+// }
 
 // POST REQUESTS
 
-// PDFs files are loaded onto the server
+// we wait for the files to finish downloading from the mongoDB server
+async function writeFile(downloadStream, pdfFilePath) {
+  return new Promise((resolve, reject) => {
+    downloadStream.pipe(
+      fs
+        .createWriteStream(pdfFilePath)
+        .on("finish", () => {
+          resolve(true);
+        })
+        .on("error", reject)
+    );
+  });
+}
+// PDFs files are sent to the database server, then loaded onto the server
 // PDF info array is stored and info about the number of pages is added asynchronously
-app.post("/", upload.array("pdf-files"), async (req, res) => {
-  const pdfFiles = req.files;
+app.post("/", upload.array("pdf-files"), (req, res) => {
   // IIFE invoked as current scope cannot be async
-  await (async () => {
+  (async () => {
+    const pdfFiles = req.files;
     for (let i = 0; i < pdfFiles.length; i++) {
-      const pdfBuffer = fs.readFileSync(pdfFiles[i].path);
+      const pdfFilePath = "\\tmp/" + pdfFiles[i].filename;
+      // open a stream to the mongoDB server
+      const downloadStream = await bucket.openDownloadStream(
+        ObjectId(pdfFiles[i].id)
+      );
+      // write to our server from the open stream
+      await writeFile(downloadStream, pdfFilePath);
+      pdfFiles[i].path = pdfFilePath;
+      // count the pages from the temporary copy of file
+      const pdfBuffer = await fsp.readFile(pdfFiles[i].path);
       const pdfFilePages = await countPages.PdfCounter.count(pdfBuffer);
-      pdfFiles[i]["pages"] = pdfFilePages;
+      pdfFiles[i].pages = pdfFilePages;
     }
+
     if (req.body.submit === "Quick Merge") {
       (async () => {
         for (let i = 0; i < pdfFiles.length; i++) {
@@ -73,7 +175,7 @@ app.post("/", upload.array("pdf-files"), async (req, res) => {
         }
         // Buffer is reset as sometimes previous session files were loaded
         merger.reset();
-        deleteMergedPDF();
+        deleteMergedPDF(req);
         res.redirect("openpdf");
       })();
     } else {
@@ -89,7 +191,7 @@ const deleteMergedPDF = () => {
     if (fs.existsSync(`/tmp/merged.pdf`)) {
       await fsp.unlink(`/tmp/merged.pdf`);
     }
-  }, 10000);
+  }, 30000);
 };
 
 // PDF info array and page numbers arrays is read from the current session
@@ -113,20 +215,29 @@ app.post("/filters", (req, res) => {
     } else {
       // Concatenating all files one by one
       for (let i = 0; i < pdfFiles.length; i++) {
+        if (!fs.existsSync(pdfFiles[i].path)) {
+          // res.render("failure", { errorMessage: "One of user files lost" });
+          const pdfFilePath = "\\tmp/" + pdfFiles[i].filename;
+          // open a stream to the mongoDB server
+          const downloadStream = await bucket.openDownloadStream(
+            ObjectId(pdfFiles[i].id)
+          );
+          // write to our server from the open stream
+          await writeFile(downloadStream, pdfFilePath);
+        }
         const range = startingPageNumbers[i] + "-" + endingPageNumbers[i];
         await merger.add(pdfFiles[i].path, range);
+        // delete the files locally and from database since they're used
+        await bucket.delete(ObjectId(pdfFiles[i].id));
+        await fsp.unlink(pdfFiles[i].path);
       }
     }
     await merger.save(`/tmp/merged.pdf`);
-    for (let i = 0; i < pdfFiles.length; i++) {
-      // Deleting files as no longer needed
-      await fsp.unlink(pdfFiles[i].path);
-    }
     // Buffer is reset as sometimes previous session files were loaded
     merger.reset();
     req.session.pdfFilesInfo = [];
-    deleteMergedPDF();
     res.render("success");
+    deleteMergedPDF();
   })();
 });
 
